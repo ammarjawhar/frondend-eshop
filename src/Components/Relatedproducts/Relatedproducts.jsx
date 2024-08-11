ac6a0dafb6e6d06644ed87309cc24e6bf04302cb
ammargoher94@mail.com <ammargoher94@mail.com>
2024-08-11 13:55:56 +0300

    add realted component

--- src/Components/Relatedproducts/Relatedproducts.jsx
+++ src/Components/Relatedproducts/Relatedproducts.jsx
@@ -1,8 +1,25 @@
 import './Relatedproducts.css';
 import data from '../Assets/data.js';
 import Item from '../Item/Item';
+import { useContext, useEffect, useState } from 'react';
+import { ShopContext } from '../../Context/ShopContext';
 
 const Relatedproducts = () => {
+  const { BASE_URL } = useContext(ShopContext);
+  const [data, setData] = useState([]);
+
+  const fetchRelatedProducts = async () => {
+    const res = await axios.get(`${BASE_URL}/api/product/related`);
+    if (res.data.success) {
+      setData(res.data.data);
+    } else {
+      console.log('error');
+    }
+  };
+
+  useEffect(() => {
+    fetchRelatedProducts();
+  }, []);
   return (
     <div className="related-items">
       <h1>Related Products</h1>
